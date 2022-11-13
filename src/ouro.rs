e30a5a81c73bc6b14777d5ef2a6e0f07cbafd710
jmhrpr <harper.jme@gmail.com>
2022-11-13 16:28:07 +0000

    chore: clean up dead code

--- src/ouro.rs
+++ src/ouro.rs
@@ -1,12 +1,11 @@
 use anyhow::{bail, Result};
 use clap::ValueEnum;
-use pallas::ledger::traverse::MultiEraHeader;
 use pallas::network::miniprotocols::{
-    blockfetch, chainsync, handshake, Point, MAINNET_MAGIC, PREVIEW_MAGIC, PRE_PRODUCTION_MAGIC,
+    blockfetch, handshake, Point, MAINNET_MAGIC, PREVIEW_MAGIC, PRE_PRODUCTION_MAGIC,
 };
 use pallas::network::multiplexer::StdChannel;
 use pallas::network::multiplexer::{bearers::Bearer, StdPlexer};
-use tracing::{debug, info, warn};
+use tracing::debug;
 
 #[derive(Copy, Clone, PartialEq, Eq, ValueEnum)]
 pub enum Network {
@@ -16,7 +15,6 @@ pub enum Network {
 }
 
 pub struct Ouroboros {
-    chainsync: chainsync::N2NClient<StdChannel>,
     blockfetch: blockfetch::Client<StdChannel>,
 }
 
@@ -44,7 +42,6 @@ impl Ouroboros {
         let mut plexer = StdPlexer::new(bearer);
         let channel0 = plexer.use_channel(0);
         let channel3 = plexer.use_channel(3);
-        let channel2 = plexer.use_channel(2);
 
         plexer.muxer.spawn();
         plexer.demuxer.spawn();
@@ -52,9 +49,7 @@ impl Ouroboros {
         // execute the required handshake against the relay
         let mut client = handshake::N2NClient::new(channel0);
 
-        let confirmation = client
-            .handshake(handshake::n2n::VersionTable::v7_and_above(magic))
-            .unwrap();
+        let confirmation = client.handshake(handshake::n2n::VersionTable::v7_and_above(magic))?;
 
         match confirmation {
             handshake::Confirmation::Accepted(v, _) => {
@@ -66,46 +61,13 @@ impl Ouroboros {
         }
 
         Ok(Ouroboros {
-            chainsync: chainsync::Client::new(channel2),
             blockfetch: blockfetch::Client::new(channel3),
         })
-    }
-
-    pub fn get_tip(&mut self) -> Result<Point, chainsync::Error> {
-        self.chainsync.intersect_tip()
     }
 
     pub fn get_block_bytes(&mut self, point: Point) -> Result<Vec<u8>, blockfetch::Error> {
         let block_bytes = self.blockfetch.fetch_single(point.clone())?;
 
         Ok(block_bytes)
-    }
-
-    pub fn get_next_block_bytes(&mut self) -> Result<Vec<u8>> {
-        loop {
-            let msg = match self.chainsync.has_agency() {
-                true => self.chainsync.request_next()?,
-                false => self.chainsync.recv_while_can_await()?,
-            };
-
-            match msg {
-                chainsync::NextResponse::RollForward(h, _) => {
-                    let header =
-                        MultiEraHeader::decode(h.variant, h.byron_prefix.map(|x| x.0), &h.cbor)?;
-
-                    let point = Point::Specific(header.slot(), header.hash().to_vec());
-
-                    debug!("chainsync rollforward ({:?})", point);
-
-                    return Ok(self.get_block_bytes(point)?);
-                }
-                chainsync::NextResponse::RollBackward(p, _) => {
-                    warn!("chainsync rollback ({:?})", p);
-                }
-                chainsync::NextResponse::Await => {
-                    info!("awaiting next block...");
-                }
-            }
-        }
     }
 }
